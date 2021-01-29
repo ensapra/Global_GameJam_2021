bd81815bc48f2c31be69f8f2a07c6ee09c9f2819
Andy Vickers <andycvickers@hotmail.com>
2021-01-29 23:16:57 +0000

    AI now detected and chases player - use prefabs

---
+++ GlobalGameJam/Assets/Scripts/AIChangeFieldOfViewColour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this works - but as soon as player leavers area of field of view, the enemy will return to normal 
+//replaced with AIChasePlayer.cs 
+
+public class AIChangeFieldOfViewColour : MonoBehaviour
+{
+/*    FieldOfView fieldOfView;
+    [Tooltip("needs to be the mesh of the field of view object")] [SerializeField] MeshRenderer meshRenderer;
+    [SerializeField] Material seeTarget;
+    Material normalMat;
+
+    void Awake()
+    {
+        fieldOfView = GetComponentInChildren<FieldOfView>();
+        normalMat = meshRenderer.material;
+    }
+
+    void Update()
+    {
+
+        if (fieldOfView.visibleTargets.Count > 1)
+            meshRenderer.material = seeTarget;
+        else
+            meshRenderer.material = normalMat;
+    }*/
+}
