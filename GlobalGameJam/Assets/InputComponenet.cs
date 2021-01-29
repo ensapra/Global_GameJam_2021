cb8eeb7c244ba41dc968d97fa5a8ff8d7eab94d6
DarkSapra <pausolsonaboada@gmail.com>
2021-01-29 21:03:34 +0100

    Player Controller

---
+++ GlobalGameJam/Assets/InputComponenet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputComponenet : MonoBehaviour
+{
+    float xInput;
+    float yInput;
+    bool running;
+    float mouseMovement;
+
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        running = Input.GetKey(KeyCode.LeftShift);
+    }
+    public float getXinput()
+    {
+        return xInput;
+    }
+    public float getYinput()
+    {
+        return yInput;
+    }
+}
--- GlobalGameJam/Assets/Scripts/MovementComponent.cs
+++ GlobalGameJam/Assets/Scripts/MovementComponent.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class MovementComponent : MonoBehaviour
 {
     Rigidbody rb;
+    InputComponenet input;
+    public float speed;
+    public float maxVelocity;
+    public float smoothVelocityGainSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        input = gameObject.GetComponent<InputComponenet>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MovePlayer();
     }
+    void MovePlayer()
+    {
+        Vector3 movementVector = new Vector3(input.getXinput(), 0 , input.getYinput());
+        Vector3 velocityVector = Vector3.Lerp(rb.velocity, movementVector.normalized*speed, Time.deltaTime*smoothVelocityGainSpeed*10);
+        velocityVector = Vector3.ClampMagnitude(velocityVector, maxVelocity);
+        velocityVector.y = rb.velocity.y;
+        rb.velocity = velocityVector;
+    }
+
 }
